0b5b212fdb28ec31d5ec82db43342b044d6cc0bc
= <=>
2021-12-05 21:36:59 +0200

    fixed ER% main stat processing issue

--- text.py
+++ text.py
@@ -13,7 +13,7 @@ def readFromImage(path_to_image):
         width, height = img.size
         setImg = img.crop((1,1, width, 50))
         subs = img.crop((73, 270, 368, 480))
-        mainInfo = img.crop((1,110,240, 200))
+        mainInfo = img.crop((1,110,260, 200))
         aTypeIMG = img.crop((7, 50, 195, 100))
         pytesseract.tesseract_cmd = path_to_tesseract
         
@@ -22,13 +22,16 @@ def readFromImage(path_to_image):
         maintess = pytesseract.image_to_string(mainInfo)
         aTypeTess = pytesseract.image_to_string(aTypeIMG)
         found = False
-        for x in setData.values.flatten():
-            if found == True: break
-            for j in setTess.split(" "):
-                if j in x:
-                    found = True
-                    setTess = setData[setData == x].dropna(axis = 1, how="all").columns[0]
-                    break
+        if setTess in setData.values.flatten():
+            found = True
+        else :
+            for x in setData.values.flatten():
+                if found == True: break
+                for j in setTess.split(" "):
+                    if j in x:
+                        found = True
+                        setTess = setData[setData == x].dropna(axis = 1, how="all").columns[0]
+                        break
 
         subStatStrings = set(subTess.split("\n")).difference(("", " ", "  "))
         mainStrings = [x for x in maintess.split("\n") if x not in ["", " ", "  "]]
